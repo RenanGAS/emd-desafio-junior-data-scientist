f28f6d35d6b166de4414be8b0f0c10630555e235
RenanGAS <renansakaoki@gmail.com>
2024-02-15 16:38:42 -0300

    feat: customize the website's menu and add a gitignore

---
+++ menu.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+def menu():
+    st.sidebar.markdown(
+        """
+        <style>
+        div[data-testid="stSidebarContent"] {
+            background-image: linear-gradient(#02487c, #02487c);
+            color: white;
+            height: 100%;
+            width: 240px;
+        }
+        .st-emotion-cache-pkbazv {
+            color: white;
+        }
+        button[title="View fullscreen"]{
+            visibility: hidden;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
+    st.sidebar.image('assets/logo_rj_prefeitura.png')
+    st.sidebar.page_link("Inicio.py", label="Início")
+    st.sidebar.page_link("pages/0_Sobre_o_1746.py", label="Sobre o 1746")
+    st.sidebar.page_link("pages/1_Desafio.py", label="Desafio")
+    st.sidebar.page_link("pages/2_Servicos_do_1746.py", label="Serviços do 1746")
