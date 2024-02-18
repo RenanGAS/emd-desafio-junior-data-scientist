05aff3e4c5831087916dc0d006dd21db9b546506
Renan <68087317+RenanGAS@users.noreply.github.com>
2024-02-18 01:23:31 -0300

    chore: change execution instructions reference

--- analise_sql.sql
+++ analise_sql.sql
@@ -1,5 +1,5 @@
 -- Desafio Técnico
--- Recomenda-se realizar a execução dos scripts na plataforma **BigQuery**. As instruções para execução estão em [InstrucoesExecucao.md](InstrucoesExecucao.md)
+-- Recomenda-se realizar a execução dos scripts na plataforma **BigQuery**. As instruções para execução estão em InstrucoesExecucao.md
 
 -- 1. Quantos chamados foram abertos no dia 01/04/2023?
 
