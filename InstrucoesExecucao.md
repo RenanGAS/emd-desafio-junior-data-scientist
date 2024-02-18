aac6f9c36d1cf98bf8c6a62f106c279b8783fa73
RenanGAS <renansakaoki@gmail.com>
2024-02-18 01:15:53 -0300

    feat: add headers and execution instructions for the answers' files

--- InstrucoesExecucao.md
+++ InstrucoesExecucao.md
@@ -1,6 +1,16 @@
 ## Instruções de Execução
 
-### Para execução local
+### Para execução dos arquivos `analise_sql.sql` e `analise_python.ipynb`
+
+- As soluções em **SQL** podem ser executadas na plataforma **BigQuery**
+    - Passo a passo em [docs.dados.rio](https://docs.dados.rio/tutoriais/como-acessar-dados/)
+
+- As soluções em **Python** no **Jupyter Notebook** podem ser executadas na plataforma **Google Colab**
+    - Com a realização do passo a passo em [docs.dados.rio](https://docs.dados.rio/tutoriais/como-acessar-dados/), basta alterar
+    o parâmetro `billing_project_id` nas quatro `queries`, para o `Id` do projeto criado no **Google Cloud Platform**.
+    - Deve-se aceitar os termos de uso e autorizar o acesso à conta no **Google** para executar as consultas.
+
+### Para execução local do App Streamlit
 
 - `python3 -m venv env`
 - `source env/bin/activate`
--- analise_sql.sql
+++ analise_sql.sql
@@ -1,31 +1,34 @@
--- Quantos chamados foram abertos no dia 01/04/2023?
+-- Desafio Técnico
+-- Recomenda-se realizar a execução dos scripts na plataforma **BigQuery**. As instruções para execução estão em [InstrucoesExecucao.md](InstrucoesExecucao.md)
+
+-- 1. Quantos chamados foram abertos no dia 01/04/2023?
 
 SELECT "01/04/2023" AS Dia, COUNT(*) AS Numero FROM `datario.administracao_servicos_publicos.chamado_1746` WHERE data_particao = "2023-04-01" AND CAST(data_inicio AS DATE) = "2023-04-01";
 
--- Qual o tipo de chamado que teve mais reclamações no dia 01/04/2023?
+-- 2. Qual o tipo de chamado que mais teve chamados abertos no dia 01/04/2023?
 
-SELECT tipo AS Tipo, SUM(reclamacoes) AS Numero FROM `datario.administracao_servicos_publicos.chamado_1746` WHERE data_particao = "2023-04-01" AND CAST(data_inicio AS DATE) = "2023-04-01" GROUP BY tipo ORDER BY Numero DESC LIMIT 1;
+SELECT tipo AS Tipo, COUNT(*) AS Numero FROM `datario.administracao_servicos_publicos.chamado_1746` WHERE data_particao = "2023-04-01" AND CAST(data_inicio AS DATE) = "2023-04-01" GROUP BY tipo ORDER BY Numero DESC LIMIT 1;
 
--- Quais os nomes dos 3 bairros que mais tiveram chamados abertos nesse dia?
+-- 3. Quais os nomes dos 3 bairros que mais tiveram chamados abertos nesse dia?
 
 SELECT CHAMADO1746.id_bairro AS ID, BAIRRO.nome AS Nome, CHAMADO1746.NumeroChamados FROM (
   SELECT id_bairro, COUNT(*) as NumeroChamados FROM `datario.administracao_servicos_publicos.chamado_1746` WHERE data_particao = "2023-04-01" AND CAST(data_inicio AS DATE) = "2023-04-01" GROUP BY id_bairro ORDER BY NumeroChamados DESC LIMIT 3
 ) as CHAMADO1746, `datario.dados_mestres.bairro` as BAIRRO WHERE CHAMADO1746.id_bairro = BAIRRO.id_bairro;
 
--- Qual o nome da subprefeitura com mais chamados abertos nesse dia?
+-- 4. Qual o nome da subprefeitura com mais chamados abertos nesse dia?
 
 SELECT BAIRRO.subprefeitura AS Nome, COUNT(*) as Numero FROM `datario.administracao_servicos_publicos.chamado_1746` AS CHAMADO1746 INNER JOIN `datario.dados_mestres.bairro` AS BAIRRO ON CHAMADO1746.id_bairro = BAIRRO.id_bairro WHERE CHAMADO1746.data_particao = "2023-04-01" AND CAST(CHAMADO1746.data_inicio AS DATE) = "2023-04-01" GROUP BY BAIRRO.subprefeitura ORDER BY Numero DESC LIMIT 1;
 
--- Existe algum chamado aberto nesse dia que não foi associado a um bairro ou subprefeitura na tabela de bairros? Se sim, por que isso acontece?
+-- 5. Existe algum chamado aberto nesse dia que não foi associado a um bairro ou subprefeitura na tabela de bairros? Se sim, por que isso acontece?
 
 SELECT tipo, subtipo, categoria, id_bairro FROM `datario.administracao_servicos_publicos.chamado_1746` WHERE data_particao = "2023-04-01" AND CAST(data_inicio AS DATE) = "2023-04-01" AND id_bairro IS NULL;
 SELECT tipo, subtipo, categoria FROM `datario.administracao_servicos_publicos.chamado_1746` AS CHAMADO1746 INNER JOIN `datario.dados_mestres.bairro` AS BAIRRO ON CHAMADO1746.id_bairro = BAIRRO.id_bairro WHERE CHAMADO1746.data_particao = "2023-04-01" AND CAST(CHAMADO1746.data_inicio AS DATE) = "2023-04-01" AND BAIRRO.subprefeitura = "";
 
--- Quantos chamados com o subtipo "Perturbação do sossego" foram abertos desde 01/01/2022 até 31/12/2023 (incluindo extremidades)?
+-- 6. Quantos chamados com o subtipo "Perturbação do sossego" foram abertos desde 01/01/2022 até 31/12/2023 (incluindo extremidades)?
 
 SELECT COUNT(*) AS Numero FROM `datario.administracao_servicos_publicos.chamado_1746` WHERE (data_particao BETWEEN "2022-01-01" AND "2023-12-01") AND subtipo = "Perturbação do sossego";
 
--- Selecione os chamados com esse subtipo que foram abertos durante os eventos contidos na tabela de eventos (Reveillon, Carnaval e Rock in Rio).
+-- 7. Selecione os chamados com esse subtipo que foram abertos durante os eventos contidos na tabela de eventos (Reveillon, Carnaval e Rock in Rio).
 
 -- Script com as datas colocadas manualmente
 SELECT "Carnaval" AS Evento, id_chamado, tipo, subtipo, id_bairro, situacao, tipo_situacao, status FROM `datario.administracao_servicos_publicos.chamado_1746` WHERE data_particao = "2023-02-01" AND (CAST(data_inicio AS DATE) BETWEEN "2023-02-18" AND "2023-02-21") AND subtipo = "Perturbação do sossego"
@@ -41,7 +44,7 @@ SELECT EVENTOS.evento, CHAMADO1746.id_chamado, CHAMADO1746.tipo, CHAMADO1746.sub
 UNION ALL
 SELECT EVENTOS.evento, CHAMADO1746.id_chamado, CHAMADO1746.tipo, CHAMADO1746.subtipo, CHAMADO1746.id_bairro, CHAMADO1746.situacao, CHAMADO1746.tipo_situacao, CHAMADO1746.status FROM `datario.administracao_servicos_publicos.chamado_1746` AS CHAMADO1746 INNER JOIN `datario.turismo_fluxo_visitantes.rede_hoteleira_ocupacao_eventos` AS EVENTOS ON (CAST(CHAMADO1746.data_inicio AS DATE) BETWEEN EVENTOS.data_inicial AND EVENTOS.data_final) WHERE CHAMADO1746.data_particao = "2022-09-01" AND EVENTOS.evento = "Rock in Rio" AND subtipo = "Perturbação do sossego";
 
--- Quantos chamados desse subtipo foram abertos em cada evento?
+-- 8. Quantos chamados desse subtipo foram abertos em cada evento?
 
 -- Script com as datas colocadas manualmente
 SELECT "Carnaval" AS Evento, COUNT(*) AS Numero FROM `datario.administracao_servicos_publicos.chamado_1746` WHERE data_particao = "2023-02-01" AND (CAST(data_inicio AS DATE) BETWEEN "2023-02-18" AND "2023-02-21") AND subtipo = "Perturbação do sossego"
@@ -66,7 +69,7 @@ SELECT "Rock in Rio" AS Evento, COUNT(*) AS Numero
     INNER JOIN `datario.turismo_fluxo_visitantes.rede_hoteleira_ocupacao_eventos` AS EVENTOS ON (CAST(CHAMADO1746.data_inicio AS DATE) BETWEEN EVENTOS.data_inicial AND EVENTOS.data_final) 
     WHERE CHAMADO1746.data_particao = "2022-09-01" AND EVENTOS.evento = "Rock in Rio" AND subtipo = "Perturbação do sossego";
 
--- Qual evento teve a maior média diária de chamados abertos desse subtipo?
+-- 9. Qual evento teve a maior média diária de chamados abertos desse subtipo?
 
 -- Solução 1
 SELECT Evento, Media FROM (
@@ -100,7 +103,7 @@ SELECT "Rock in Rio" AS Evento, COUNT(*)/DiasRockRio AS Media FROM `datario.admi
     WHERE data_particao = "2022-09-01" AND ((CAST(data_inicio AS DATE) BETWEEN "2022-09-02" AND "2022-09-04") OR (CAST(data_inicio AS DATE) BETWEEN "2022-09-08" AND "2022-09-11")) AND subtipo = "Perturbação do sossego"
 ) ORDER BY Media DESC LIMIT 1;
 
--- Compare as médias diárias de chamados abertos desse subtipo durante os eventos específicos (Reveillon, Carnaval e Rock in Rio) e a média diária de chamados abertos desse subtipo considerando todo o período de 01/01/2022 até 31/12/2023.
+-- 10. Compare as médias diárias de chamados abertos desse subtipo durante os eventos específicos (Reveillon, Carnaval e Rock in Rio) e a média diária de chamados abertos desse subtipo considerando todo o período de 01/01/2022 até 31/12/2023.
 
 SELECT "De 01/01/2022 a 31/12/2023" AS Periodo, AVG(Numero) AS Media FROM (
   SELECT COUNT(*) AS Numero FROM `datario.administracao_servicos_publicos.chamado_1746` WHERE (data_particao BETWEEN "2022-01-01" AND "2023-12-01") AND subtipo = "Perturbação do sossego" GROUP BY CAST(data_inicio AS DATE));
